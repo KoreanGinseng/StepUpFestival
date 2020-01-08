e24e3319328fb4232c5542517e3cd3b8f9e0bc53
KoreanGinseng <49602459+KoreanGinseng@users.noreply.github.com>
2020-01-08 13:43:29 +0900

    add skeleton

--- CommandBattle/WinMain.cpp
+++ CommandBattle/WinMain.cpp
@@ -3,14 +3,39 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	// ウインドウモードで起動
+	ChangeWindowMode(TRUE);
+
+	//画面サイズの変更
+	SetGraphMode(640, 480, 16);
+
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;				// エラーが起きたら直ちに終了
 	}
 
-	while (1)
+	// 描画先画面を裏画面にする
+	SetDrawScreen(DX_SCREEN_BACK);
+	
+	while (ProcessMessage() == 0)
 	{
+		//更新
 
+
+		//描画
+		//画面クリア
+		if (ClearDrawScreen() == -1)
+		{
+			return -1;
+		}
+
+
+
+		//裏画面を表画面に反映
+		if (ScreenFlip() == -1)
+		{
+			return -1;
+		}
 	}
 
 
